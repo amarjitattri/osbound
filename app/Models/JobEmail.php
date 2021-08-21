1bfc8e3a62c004a5547d763d0d98f01c591903cc
Aashish Malethia <amalethia@gmail.com>
2021-08-21 19:10:36 +0530

    attachments in email

--- app/Models/JobEmail.php
+++ app/Models/JobEmail.php
@@ -15,6 +15,11 @@ class JobEmail extends Model
         'from',
         'to',
         'subject',
-        'message'
+        'message',
+        'attachments'
+    ];
+
+    protected $casts = [
+        'attachments' => 'array',
     ];
 }
